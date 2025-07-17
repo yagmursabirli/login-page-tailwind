78f1fe049d0f19fdefe95c41e0896f7645fddbf3
yagmursabirli <yagmursabirli35@gmail.com>
2025-07-17 17:16:35 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./script.js"],
+  theme: {
+    extend: {
+      backgroundImage:{
+        'mobile': "url('./images/bg-intro-mobile.png')",
+        'desktop': "url('./images/bg-intro-desktop.png')",
+      },
+      colors:{
+        'red400': 'red',
+        'button': 'hsl(248, 32%,49%)',
+        'claim': 'hsl(154, 59%, 51%)',
+        'buttonShadow': 'hsl(154, 73%, 43%)',
+        'terms-color': 'hsl(246, 25%, 77%)',
+      },
+      fontFamily:{
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
